4822c6db2acfb12dadd759f0cd056c4441955db9
quasar-r <87948785+quasar-r@users.noreply.github.com>
2021-07-25 19:12:15 -0400

    Update Recursion-permutation.py

--- Recursion-permutation.py
+++ Recursion-permutation.py
@@ -11,6 +11,10 @@ class Rec:
     def perm(self,nums,path):
         if len(nums) == 0:
             return self.res.append(path)
+            '''
+            in case of string 
+            return self.res.append("".join(path))
+            '''
         for i in range(0,len(nums)):
             print(nums[i])
             x = nums.pop(i)
@@ -18,6 +22,13 @@ class Rec:
             nums.insert(i,x)
 
 nums=[1,2,3]
+'''
+if nums is array of string
+nums = ["a","b","c"]
+if nums is string of alphabet
+nums="abc"
+nums = list(nums)
+'''
 obj = Rec()
 result = obj.find_perm(nums)
 print(result)
