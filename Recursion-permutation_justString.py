6285a8ac87e92d14a6c29d1642b46889283d6502
quasar-r <87948785+quasar-r@users.noreply.github.com>
2021-07-25 19:48:04 -0400

    Create Recursion-permutation_justString.py

---
+++ Recursion-permutation_justString.py
@@ -0,0 +1,22 @@
+class Rec:
+    def find_perm(self, str_s):
+        
+        return self.perm_str(str_s)
+        
+        
+
+    def perm_str(self,str_s):
+        if len(str_s) == 1:
+            return [str_s[-1]]
+        char = str_s[0]
+        set_ = self.perm_str(str_s[1:])
+        result = []
+        for each in set_:
+            for i in range(0,len(each)+1):
+                result.append(each[:i]+char+each[i:])
+        return result
+str_s = "abc"
+re = Rec()
+all_perms = re.find_perm(str_s)
+print(all_perms)
+    
