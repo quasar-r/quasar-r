273f2cd8b82fb92bfc6c8453a7fbce2e6b6198de
quasar-r <87948785+quasar-r@users.noreply.github.com>
2021-07-25 20:10:41 -0400

    Update Recursion-permutation.py

--- Recursion-permutation.py
+++ Recursion-permutation.py
@@ -2,33 +2,25 @@ class Rec:
     
     def __init__(self):
         self.res = []
-        
     def find_perm(self, nums):
         self.perm(nums,[])
         return self.res
-        
-
     def perm(self,nums,path):
         if len(nums) == 0:
             return self.res.append(path)
-            '''
-            in case of string 
-            return self.res.append("".join(path))
-            '''
         for i in range(0,len(nums)):
-            print(nums[i])
             x = nums.pop(i)
             self.perm(nums,path+[x])
+            '''
+            if nums is  string
+            nums = "abc"
+            for loop will only have below code as string are immutable so we can slice and pass substring without poping or inserting
+            perm_str(s[:i]+s[i+1:],path+s[i])
+            '''
             nums.insert(i,x)
 
 nums=[1,2,3]
-'''
-if nums is array of string
-nums = ["a","b","c"]
-if nums is string of alphabet
-nums="abc"
-nums = list(nums)
-'''
+#nums = "abc"
 obj = Rec()
 result = obj.find_perm(nums)
 print(result)
